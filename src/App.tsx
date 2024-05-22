55a2edd1b4b5dd9d29cde93eb50432b39699ac3c
cihanalici <cihan_cihan27@hotmail.com>
2024-05-22 23:43:31 +0300

    ~header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import SearchMovies from "./components/SearchMovies";
 function App() {
   return (
     <div className="container">
-      <h1 className="title">Movie Search App</h1>
+      <h1 className="title">React & TS Movie Search App</h1>
       <SearchMovies />
     </div>
   );
